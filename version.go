cb5bf45aba888a502cb9b7525e6745e9836aa486
Alexey Nadyozhny <Alexey_Nadyozhny@epam.com>
2019-08-29 15:59:16 +0300

    modified: version.go

--- version.go
+++ version.go
@@ -1,3 +1,3 @@
 package main
-const version = "20160522.0-ea0ae7b"
-const commit = "ea0ae7b1faabd4e511d16d75142d97c683d64646"
+const version = "20190829.0-cb490b9"
+const commit = "cb490b9326719e9b3e7454e4a71eb43b48145860"
